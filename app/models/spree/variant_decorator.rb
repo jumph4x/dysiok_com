30c7831688019879dcb726d40bb1b5ed2eb86f36
Denis <visible@jumph4x.net>
2017-03-31 21:19:09 -0700

    Variant automation

---
+++ app/models/spree/variant_decorator.rb
@@ -0,0 +1,41 @@
+module Spree
+  Variant.class_eval do
+    before_validation :set_sku
+    before_validation :set_price
+
+    def size_option
+      option_values.find{|v| v.option_type.name == 'size'}
+    end
+
+    def pretty_size
+      size_option && size_option.presentation
+    end
+
+    def size
+      size_option && size_option.name
+    end
+
+    def determine_price size
+      {
+        "8x12" => 10, "12x18" => 25, "16x24" => 50, "24x36" => 115,
+        "6x12" => 10, "10x20" => 25, "15x30" => 40, "20x40" => 100
+      }[size]
+    end
+
+    def padded_price price
+      price * 1.25
+    end
+
+  private
+
+    def set_sku
+      return true if is_master?
+      self.sku = "#{product.sku}-#{size}"
+    end
+
+    def set_price
+      return true if is_master?
+      self.price = determine_price(size)
+    end
+  end
+end

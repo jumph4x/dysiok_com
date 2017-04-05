ea13b42300e85454d25f9e0e5faab39c9164a690
Denis <visible@jumph4x.net>
2017-04-05 02:06:40 -0700

    Fix Checkout JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require jquery.payment
+//= require jquery.validate/jquery.validate.min
+//= require spree
+//= require spree/frontend/cart
+//= require spree/frontend/checkout
+//= require spree/frontend/checkout/address
+//= require spree/frontend/checkout/payment
+//= require spree/frontend/product
 //= require_tree .
 
 $(function(){ $(document).foundation(); });

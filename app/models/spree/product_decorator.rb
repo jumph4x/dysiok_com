6bbc10a7aae83dab64914107cc50fe061b154c4e
Denis <visible@jumph4x.net>
2017-04-06 16:13:51 -0700

    SKU generation

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,11 @@
 module Spree
   Product.class_eval do
+    before_validation :set_sku
 
+    def set_sku
+      return if self.sku.present?
+
+      self.sku = self.name.parameterize.upcase
+    end
   end
 end
